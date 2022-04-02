97d386b6f1bd7e3b146ed2347ffba628ce3fa6fa
jicheol82 <jicheol82@gmail.com>
2022-04-02 17:15:03 +0900

    1. add Photo in partsmarket

--- users/models.py
+++ users/models.py
@@ -39,7 +39,7 @@ class Company(AbstractItem):
 # 사용자 모델 생성하기
 class User(AbstractUser):
     # username은 15.1에서 form.py에서 save()를 overide 하여 email로 저장한다
-    profile_img = models.ImageField(blank=True)
+    profile_img = models.ImageField(upload_to="profile_img", blank=True)
     nickname = models.CharField(max_length=20)
     contact_number = models.CharField(max_length=20, blank=True)
     company_email = models.EmailField(blank=True)
