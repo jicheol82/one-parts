74e83dae5eab06378014465ef0e0bc6474017133
jicheol82 <jicheol82@gmail.com>
2022-04-06 23:14:35 +0900

    5. virtual pool search - 검색결과 표시 및 pagination

--- virtualpools/views.py
+++ virtualpools/views.py
@@ -10,17 +10,22 @@ class VirtualPoolView(ListView):
     paginate_orphans = 5
     ordering = "created"
 
+    # 다중쿼리를 이용함으로써 검색필드를 제거함
     def get_queryset(self):
         queryset = super().get_queryset()
-        range = self.request.GET.get("range")
-        search_kwarg = self.request.GET.get("search_kwarg")
-        if search_kwarg is not None:
+        search = self.request.GET.get("search")
+        if search is not None:
             queryset = queryset.filter(
-                Q(my_stock__stock_name__contains=search_kwarg)
-                | Q(my_stock__maker__name__contains=search_kwarg)
-                | Q(my_stock__model_name__contains=search_kwarg)
+                Q(my_stock__stock_name__contains=search)
+                | Q(my_stock__maker__name__contains=search)
+                | Q(my_stock__model_name__contains=search)
             )
         return queryset
 
-
-# 검색결과에 대한 pagination
+    # 검색어 창에 검색어를 남기고, 검색결과에 대한 pagination을 위해 context를 추가
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search = self.request.GET.get("search")
+        if search:
+            context["input"] = search
+        return context
