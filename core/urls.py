ec095bf6ef9487a3f89f4bc2dde2de69f60890e6
jicheol82 <jicheol82@gmail.com>
2022-04-07 15:58:23 +0900

    7. login/out 완료

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ app_name = "core"
 urlpatterns = [
     path("", views.home, name="home"),
     path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import FormView
 from core.forms import *
 
 
 def home(request):
-    # 아마 여기서 로그인 체크를 해야겠지?
-
+    # 정적인 페이지를 보여준다
     return render(request, "home/home.html")
 
 
 class LoginView(FormView):
     form_class = LoginForm
     template_name = "home/login.html"
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
